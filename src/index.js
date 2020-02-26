ee83d7f7b68a357852d6a2bd74d3c6e86112f2c3
Energon89 <podymov89@gmail.com>
2020-02-26 17:08:51 +0300

    feat: add reverse function w

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const stringNumber = new String(n);
+    return stringNumber.replace('-', '').split('').reverse().join('');
 }
